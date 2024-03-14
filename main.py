e3bc0126a6c44228f51e73d441341d01d06bebc4
Домовой <143743445+maxz2024@users.noreply.github.com>
2024-03-14 06:29:24 +0000

    build

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ app.include_router(author.router)
 app.include_router(film.router)
 app.include_router(genre.router)
 
+import uvicorn
+
+if __name__ == "__main__":
+  uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
 
 
